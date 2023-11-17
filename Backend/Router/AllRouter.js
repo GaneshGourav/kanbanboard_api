54ab0842a76b174a6c29825565b377e8a783afbe
Ganesh Kumar <mrganeshofficial8@gmail.com>
2023-11-17 20:52:04 +0530

    Issue fixed

--- Backend/Router/AllRouter.js
+++ Backend/Router/AllRouter.js
@@ -25,25 +25,25 @@ boardRouter.get("/", async(req,res)=>{
     }
 })
 
-boardRouter.post("/:id/row/:rowid/task",async(req,res)=>{
-    try {
-        const {title,description,status}= req.body;
-        const id = req.params.id;
-        const rowid = req.params.rowid;
-        const board = await BoardModel.findById(id);
-        if(!board){
-            return res.status(404).json({"msg":"board not found"})
-        }
+// boardRouter.post("/:id/row/:rowid/task",async(req,res)=>{
+//     try {
+//         const {title,description,status}= req.body;
+//         const id = req.params.id;
+//         const rowid = req.params.rowid;
+//         const board = await BoardModel.findById(id);
+//         if(!board){
+//             return res.status(404).json({"msg":"board not found"})
+//         }
 
-        const row = board.row.id(rowid);
-        if(!row){
-            return res.status(404).json({"msg":"board not found"})
-        }
-row.task.push({title,description,status});
-await board.save();
-res.status(200).json({"msg":board})
+//         const row = board.row.id(rowid);
+//         if(!row){
+//             return res.status(404).json({"msg":"board not found"})
+//         }
+// row.task.push({title,description,status});
+// await board.save();
+// res.status(200).json({"msg":board})
         
-    } catch (error) {
-        res.status(500).json({"msg":"Internal Server Error"})
-    }
-})
+//     } catch (error) {
+//         res.status(500).json({"msg":"Internal Server Error"})
+//     }
+// })
